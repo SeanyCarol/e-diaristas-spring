8692a9039aa00d877efc2153e7a8092dd96c6d19
SeanyCarol <seany@ice.ufjf.br>
2021-10-12 19:57:43 -0300

    Implement validation with Bean Validation

--- src/main/java/br/com/treinaweb/ediaristas/controllers/HousekeeperController.java
+++ src/main/java/br/com/treinaweb/ediaristas/controllers/HousekeeperController.java
@@ -1,7 +1,10 @@
 package br.com.treinaweb.ediaristas.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,7 +40,11 @@ public class HousekeeperController {
   }
 
   @PostMapping("/cadastrar")
-  public String register(Housekeeper housekeeper) {
+  public String register(@Valid Housekeeper housekeeper, BindingResult result) {
+    if (result.hasErrors()) {
+      return "admin/housekeepers/form";
+    }
+
     repository.save(housekeeper);
 
     return "redirect:/admin/diaristas";
@@ -53,7 +60,11 @@ public class HousekeeperController {
   }
 
   @PostMapping("/{id}/editar")
-  public String update(@PathVariable Long id, Housekeeper housekeeper) {
+  public String update(@PathVariable Long id, @Valid Housekeeper housekeeper, BindingResult result) {
+    if (result.hasErrors()) {
+      return "admin/housekeepers/form";
+    }
+
     repository.save(housekeeper);
 
     return "redirect:/admin/diaristas";
--- src/main/java/br/com/treinaweb/ediaristas/models/Housekeeper.java
+++ src/main/java/br/com/treinaweb/ediaristas/models/Housekeeper.java
@@ -6,6 +6,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.br.CPF;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,39 +31,64 @@ public class Housekeeper {
   @EqualsAndHashCode.Include
   private Long id;
 
+  @NotNull
+  @Size(min = 3, max = 100)
   @Column(nullable = false, length = 100)
   private String fullname;
 
+  @NotNull
+  @Size(min = 11, max = 14)
+  @CPF
   @Column(nullable = false, unique = true, length = 11)
   private String cpf;
 
+  @NotNull
+  @NotEmpty
+  @Email
   @Column(nullable = false, unique = true)
   private String email;
 
+  @NotNull
+  @Size(min = 11, max = 15)
   @Column(nullable = false, length = 11)
   private String telephone;
 
+  @NotNull
+  @NotEmpty
   @Column(nullable = false)
   private String address;
 
+  @NotNull
+  @NotEmpty
   @Column(nullable = false)
   private String number;
 
+  @NotNull
+  @NotEmpty
   @Column(nullable = false)
   private String neighborhood;
 
   @Column(nullable = true)
   private String addressComplement;
 
+  @NotNull
+  @Size(min = 8, max = 9)
   @Column(nullable = false, length = 8)
   private String cep;
 
+  @NotNull
+  @NotEmpty
   @Column(nullable = false)
   private String city;
 
+  @NotNull
+  @NotEmpty
+  @Size(min = 2, max = 2)
   @Column(nullable = false, length = 2)
   private String state;
 
+  @NotNull
+  @NotEmpty
   @Column(nullable = false)
   private String ibgeCode;
 }
