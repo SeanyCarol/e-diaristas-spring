2f24192a7309bd18cdc3f8033acf536428dc2751
SeanyCarol <seany@ice.ufjf.br>
2021-10-11 23:36:44 -0300

    Create the housekeeper registration form display action

--- src/main/java/br/com/treinaweb/ediaristas/controllers/HousekeeperController.java
+++ src/main/java/br/com/treinaweb/ediaristas/controllers/HousekeeperController.java
@@ -1,10 +1,22 @@
 package br.com.treinaweb.ediaristas.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.treinaweb.ediaristas.models.Housekeeper;
 
 @Controller
 @RequestMapping("/admin/diaristas")
 public class HousekeeperController {
 
+  @GetMapping("/cadastrar")
+  public ModelAndView register() {
+    var modelAndView = new ModelAndView("admin/housekeepers/form");
+
+    modelAndView.addObject("housekeeper", new Housekeeper());
+
+    return modelAndView;
+  }
 }
