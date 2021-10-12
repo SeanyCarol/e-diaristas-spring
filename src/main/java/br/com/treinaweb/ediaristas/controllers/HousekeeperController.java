4821932cda7c3d4eb344e4a0e6606d01e5ab2bd3
SeanyCarol <seany@ice.ufjf.br>
2021-10-12 00:04:51 -0300

    Create the housekeeper registration action

--- src/main/java/br/com/treinaweb/ediaristas/controllers/HousekeeperController.java
+++ src/main/java/br/com/treinaweb/ediaristas/controllers/HousekeeperController.java
@@ -1,15 +1,21 @@
 package br.com.treinaweb.ediaristas.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.treinaweb.ediaristas.models.Housekeeper;
+import br.com.treinaweb.ediaristas.repositories.HousekeeperRepository;
 
 @Controller
 @RequestMapping("/admin/diaristas")
 public class HousekeeperController {
+
+  @Autowired
+  private HousekeeperRepository repository;
 
   @GetMapping("/cadastrar")
   public ModelAndView register() {
@@ -18,5 +24,12 @@ public class HousekeeperController {
     modelAndView.addObject("housekeeper", new Housekeeper());
 
     return modelAndView;
+  }
+
+  @PostMapping("/cadastrar")
+  public String register(Housekeeper housekeeper) {
+    repository.save(housekeeper);
+
+    return "redirect:/admin/diaristas/cadastrar";
   }
 }
