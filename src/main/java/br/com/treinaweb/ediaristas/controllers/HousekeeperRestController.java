9962ea31f6892e6b9e7e5b58eb9e2d5ad4812d72
SeanyCarol <seany@ice.ufjf.br>
2021-10-13 20:32:23 -0300

    Filter housekeepers based on IBGE code

--- src/main/java/br/com/treinaweb/ediaristas/controllers/HousekeeperRestController.java
+++ src/main/java/br/com/treinaweb/ediaristas/controllers/HousekeeperRestController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.treinaweb.ediaristas.models.Housekeeper;
 import br.com.treinaweb.ediaristas.repositories.HousekeeperRepository;
+import br.com.treinaweb.ediaristas.services.ViaCepService;
 
 @RestController
 @RequestMapping("/api/diaristas-cidade")
@@ -17,8 +19,14 @@ public class HousekeeperRestController {
   @Autowired
   private HousekeeperRepository repository;
 
+  @Autowired
+  private ViaCepService viaCepService;
+
   @GetMapping
-  public List<Housekeeper> findHouseKeeperByCep() {
-    return repository.findAll();
+  public List<Housekeeper> findHouseKeeperByCep(@RequestParam String cep) {
+    var address = viaCepService.getAddressByCep(cep);
+    var ibgeCode = address.getIbge();
+
+    return repository.findByIbgeCode(ibgeCode);
   }
 }
--- src/main/java/br/com/treinaweb/ediaristas/repositories/HousekeeperRepository.java
+++ src/main/java/br/com/treinaweb/ediaristas/repositories/HousekeeperRepository.java
@@ -1,9 +1,12 @@
 package br.com.treinaweb.ediaristas.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.treinaweb.ediaristas.models.Housekeeper;
 
 public interface HousekeeperRepository extends JpaRepository<Housekeeper, Long> {
 
+  List<Housekeeper> findByIbgeCode(String ibgeCode);
 }
